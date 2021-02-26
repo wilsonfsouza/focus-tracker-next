8bfcca7afabf1579ff68099f68733d1dc768b52c
Wilson Franca <wilson.franca.92@gmail.com>
2021-02-25 20:00:49 -0800

    feat: added theme.

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -8,20 +8,6 @@ export default createGlobalStyle`
     outline: 0;
   }
 
-  :root {
-    --white: #fff;
-    --background: #F2F3F5;
-    --gray-line: #DCDDE0;
-    --text: #666666;
-    --text-highlight: #B3B9FF;
-    --title: #2E384D;
-    --red: #E83F5B;
-    --green: #4CD62B;
-    --blue: #5965E0;
-    --blue-dark: #4953B8;
-    --blue-twitter: #2AA9E0;
-  }
-
   @media(max-width: 1080px) {
   html {
     font-size: 93.75%;
@@ -35,8 +21,8 @@ export default createGlobalStyle`
   }
 
   body {
-    background: var(--white);
-    color: var(--text);
+    background: ${props => props.theme.colors.background};
+    color: ${props => props.theme.colors.text};
     -webkit-font-smoothing: antialised;
   }
 
---
+++ src/styles/theme.ts
@@ -0,0 +1,17 @@
+const theme = {
+  colors: {
+    white: '#fff',
+    background: '#F2F3F5',
+    grayLine: '#DCDDE0',
+    text: '#666666',
+    textHighlight: '#B3B9FF',
+    title: '#2E384D',
+    red: '#E83F5B',
+    green: '#4CD62B',
+    blue: '#5965E0',
+    blueDark: '#4953B8',
+    blueTwitter: '#2AA9E0'
+  }
+}
+
+export default theme;
