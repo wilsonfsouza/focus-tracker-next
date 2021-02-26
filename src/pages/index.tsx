1d62a5fbd9715e676df945661ecb353eb0b5c12b
Wilson Franca <wilson.franca.92@gmail.com>
2021-02-25 22:32:47 -0800

    feat(Home page): included page title. Added ExperienceBar, Profile, CompletedChallenges, and Countdown components.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,30 @@
 import React from 'react';
+import CompletedChallenges from '../components/CompletedChallenges';
+import CountDown from '../components/Countdown';
+import ExperienceBar from '../components/ExperienceBar';
+import Profile from '../components/Profile';
+
+import Head from 'next/head';
+
+import { Container, Section, LeftContainer } from '../styles/pages/Home';
 
 const Home: React.FunctionComponent = () => {
   return (
-    <h1>Hello</h1>
+    <Container>
+      <Head>
+        <title>Home | Focus Tracker</title>
+      </Head>
+      <ExperienceBar />
+
+      <Section>
+        <LeftContainer>
+          <Profile />
+          <CompletedChallenges />
+          <CountDown />
+        </LeftContainer>
+        <div></div>
+      </Section>
+    </Container>
   )
 }
 
