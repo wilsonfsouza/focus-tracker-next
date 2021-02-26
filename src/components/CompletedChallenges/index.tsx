e0b2a08e4c5afe9d4be81d364c34a3b1b0e3ac60
Wilson Franca <wilson.franca.92@gmail.com>
2021-02-25 22:38:38 -0800

    feat(components): created styled completed challenges base component.

---
+++ src/components/CompletedChallenges/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import { Container } from '../../styles/components/CompletedChallenges';
+
+const CompletedChallenges: React.FunctionComponent = () => {
+  return (
+    <Container>
+      <span>Completed Challenges</span>
+      <span>5</span>
+    </Container>
+  )
+}
+
+export default CompletedChallenges;
