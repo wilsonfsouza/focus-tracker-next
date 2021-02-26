5f0a2227eff103c14b7e6e43ccab10e6ed026f5c
Wilson Franca <wilson.franca.92@gmail.com>
2021-02-25 22:37:42 -0800

    feat(components): created styled countdown component with functions to start and reset a timer.

---
+++ src/components/Countdown/index.tsx
@@ -0,0 +1,74 @@
+import React, { useCallback, useState, useEffect } from 'react';
+
+import { Container, CountdownContainer, CountdownButton } from '../../styles/components/Countdown';
+
+let countdownTimeout: NodeJS.Timeout;
+
+const Countdown: React.FunctionComponent = () => {
+  const [time, setTime] = useState(0.1 * 60);
+  const [isActive, setIsActive] = useState(false);
+  const [hasFinished, setHasFinished] = useState(false);
+
+  const minutes = Math.floor(time / 60);
+  const seconds = time % 60;
+
+  const [minuteLeft, minuteRight] = String(minutes).padStart(2, '0').split('');
+  const [secondLeft, secondRight] = String(seconds).padStart(2, '0').split('');
+
+  const startCountDown = useCallback(() => {
+    setIsActive(true);
+  }, []);
+
+  const resetCountDown = useCallback(() => {
+    clearTimeout(countdownTimeout);
+    setIsActive(false);
+    setTime(0.1 * 60);
+  }, []);
+
+  useEffect(() => {
+    if (isActive && time > 0) {
+      countdownTimeout = setTimeout(() => {
+        setTime(time - 1);
+      }, 1000);
+    } else if (isActive && time === 0) {
+      setHasFinished(true);
+      setIsActive(false);
+    }
+  }, [isActive, time]);
+
+  return (
+    <Container>
+      <CountdownContainer>
+        <div>
+          <span>{minuteLeft}</span>
+          <span>{minuteRight}</span>
+        </div>
+        <span>:</span>
+        <div>
+          <span>{secondLeft}</span>
+          <span>{secondRight}</span>
+        </div>
+      </CountdownContainer>
+
+      { hasFinished ? (
+        <CountdownButton disabled>
+          Done!
+        </CountdownButton>
+      ) : (
+          <>
+            {isActive ? (
+              <CountdownButton isActive type="button" onClick={resetCountDown}>
+                Stop
+              </CountdownButton>
+            ) : (
+                <CountdownButton type="button" onClick={startCountDown}>
+                  Start
+                </CountdownButton>
+              )}
+          </>
+        )}
+    </Container>
+  );
+}
+
+export default Countdown;
---
+++ src/styles/components/Countdown.ts
@@ -0,0 +1,87 @@
+import styled, { css } from 'styled-components';
+
+interface CountdownButtonProps {
+  isActive?: boolean;
+}
+
+export const Container = styled.div``;
+
+export const CountdownContainer = styled.div`
+  display: flex;
+  align-items: center;
+  font-family: Rajdhani;
+  font-weight: 600;
+  color: ${props => props.theme.colors.title};
+
+  > div {
+    flex: 1;
+
+    display: flex;
+    align-items: center;
+    justify-content: space-evenly;
+
+    background: ${props => props.theme.colors.white};
+    box-shadow: 0 0 60px rgba(0, 0, 0, 0.05);
+    border-radius: 5px;
+    font-size: 8.5rem;
+    text-align: center;
+
+    span {
+      flex: 1;
+    }
+
+    span:first-child {
+      border-right: 1px solid #F0F1F3;
+    }
+
+    span:last-child {
+      border-left: 1px solid #F0F1F3;
+    }
+  }
+
+  > span {
+    font-size: 6.25rem;
+    margin: 0 0.5rem;
+  }
+`;
+
+export const CountdownButton = styled.button<CountdownButtonProps>`
+  width: 100%;
+  height: 5rem;
+
+  margin-top: 2rem;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  border: 0;
+  border-radius: 5px;
+
+  background: ${props => props.theme.colors.blue};
+  color: ${props => props.theme.colors.white};
+
+  font-size: 1.25rem;
+  font-weight: 600;
+  transform: background-color .2s;
+
+  &:not(:disabled):hover {
+    background: ${props => props.theme.colors.blueDark};
+  }
+
+  ${props => props.isActive && css`
+    background: ${props => props.theme.colors.white};
+    color: ${props => props.theme.colors.title};
+
+    &:not(:disabled):hover {
+      background: ${props => props.theme.colors.red};
+      color: ${props => props.theme.colors.white};
+    }
+  `}
+
+  &:disabled {
+    background: ${props => props.theme.colors.white};
+    color: ${props => props.theme.colors.text};
+    cursor: not-allowed;
+  }
+`;
