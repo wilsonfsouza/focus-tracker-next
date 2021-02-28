cca3df559ca07f4414a833fc777017ba517f37b1
Wilson Franca <wilson.franca.92@gmail.com>
2021-02-28 10:50:08 -0800

    feat(components): added completed countdown timer.

--- src/components/Countdown/index.tsx
+++ src/components/Countdown/index.tsx
@@ -1,40 +1,20 @@
-import React, { useCallback, useState, useEffect } from 'react';
+import React, { useContext } from 'react';
+import { CountdownContext } from '../../contexts/CountdownContext';
 
 import { Container, CountdownContainer, CountdownButton } from '../../styles/components/Countdown';
 
-let countdownTimeout: NodeJS.Timeout;
-
 const Countdown: React.FunctionComponent = () => {
-  const [time, setTime] = useState(0.1 * 60);
-  const [isActive, setIsActive] = useState(false);
-  const [hasFinished, setHasFinished] = useState(false);
-
-  const minutes = Math.floor(time / 60);
-  const seconds = time % 60;
+  const {
+    minutes,
+    seconds,
+    hasFinished,
+    isActive,
+    resetCountDown,
+    startCountDown
+  } = useContext(CountdownContext);
 
   const [minuteLeft, minuteRight] = String(minutes).padStart(2, '0').split('');
   const [secondLeft, secondRight] = String(seconds).padStart(2, '0').split('');
-
-  const startCountDown = useCallback(() => {
-    setIsActive(true);
-  }, []);
-
-  const resetCountDown = useCallback(() => {
-    clearTimeout(countdownTimeout);
-    setIsActive(false);
-    setTime(0.1 * 60);
-  }, []);
-
-  useEffect(() => {
-    if (isActive && time > 0) {
-      countdownTimeout = setTimeout(() => {
-        setTime(time - 1);
-      }, 1000);
-    } else if (isActive && time === 0) {
-      setHasFinished(true);
-      setIsActive(false);
-    }
-  }, [isActive, time]);
 
   return (
     <Container>
