1dc29028e8b912849de45a641d1d7f22eb87913e
Wilson Franca <wilson.franca.92@gmail.com>
2021-02-28 10:52:26 -0800

    feat(components): added profile with photo, name, and current level.

--- src/components/Profile/index.tsx
+++ src/components/Profile/index.tsx
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { ChallengesContext } from '../../contexts/ChallengesContext';
 
 import { Container } from '../../styles/components/Profile';
 
 const Profile: React.FunctionComponent = () => {
+  const { level } = useContext(ChallengesContext);
   return (
     <Container>
       <img src="https://avatars.githubusercontent.com/u/21347383?s=460&u=fdb399c92e369762d45d6495cbd2e87eef9e4d65&v=4" alt="Wilson Franca" />
@@ -10,7 +12,7 @@ const Profile: React.FunctionComponent = () => {
         <strong>Wilson Franca</strong>
         <p>
           <img src="icons/level.svg" alt="Level" />
-          Level 1
+          Level {level}
         </p>
       </div>
     </Container>
