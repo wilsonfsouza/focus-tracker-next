b6727281249b884068ee6dadf45fe88c741568c9
Wilson Franca <wilson.franca.92@gmail.com>
2021-02-28 10:40:29 -0800

    feat: pulled experience information from Challenges Context.

--- src/components/ExperienceBar/index.tsx
+++ src/components/ExperienceBar/index.tsx
@@ -1,16 +1,21 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { ChallengesContext } from '../../contexts/ChallengesContext';
 
 import { Container } from '../../styles/components/ExperienceBar';
 
 const ExperienceBar: React.FunctionComponent = () => {
+  const { currentExperience, experienceToNextLevel } = useContext(ChallengesContext);
+
+  const percentToNextLevel = Math.round(currentExperience * 100) / experienceToNextLevel;
+
   return (
     <Container>
       <span>0 px</span>
       <div>
-        <div style={{ width: '50%' }} />
-        <span style={{ left: '50%' }}>300px</span>
+        <div style={{ width: `${percentToNextLevel}%` }} />
+        <span style={{ left: `${percentToNextLevel}%` }}>{currentExperience} xp</span>
       </div>
-      <span>600 xp</span>
+      <span>{experienceToNextLevel} xp</span>
     </Container>
   );
 };
