7e1e6ca554f2325a75b7022d0186c3e638d871d8
Wilson Franca <wilson.franca.92@gmail.com>
2021-02-28 10:43:11 -0800

    feat(homepage): wrapped challenges provider around homepage since this is the only page that needs the information from this context.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,31 +1,65 @@
 import React from 'react';
+import Head from 'next/head';
+import { GetServerSideProps } from 'next';
+
 import CompletedChallenges from '../components/CompletedChallenges';
 import CountDown from '../components/Countdown';
 import ExperienceBar from '../components/ExperienceBar';
 import Profile from '../components/Profile';
+import ChallengeBox from '../components/ChallengeBox';
 
-import Head from 'next/head';
 
-import { Container, Section, LeftContainer } from '../styles/pages/Home';
+import { Container, Section, LeftContainer, RightContainer } from '../styles/pages/Home';
+import { CountdownProvider } from '../contexts/CountdownContext';
+import { ChallengesProvider } from '../contexts/ChallengesContext';
 
-const Home: React.FunctionComponent = () => {
+interface HomeProps {
+  level: number;
+  currentExperience: number;
+  challengesCompleted: number;
+}
+
+const Home: React.FunctionComponent<HomeProps> = ({ level, currentExperience, challengesCompleted }) => {
   return (
-    <Container>
-      <Head>
-        <title>Home | Focus Tracker</title>
-      </Head>
-      <ExperienceBar />
+    <ChallengesProvider
+      level={level}
+      currentExperience={currentExperience}
+      challengesCompleted={challengesCompleted}
+    >
+      <Container>
+        <Head>
+          <title>Home | Focus Tracker</title>
+        </Head>
+        <ExperienceBar />
 
-      <Section>
-        <LeftContainer>
-          <Profile />
-          <CompletedChallenges />
-          <CountDown />
-        </LeftContainer>
-        <div></div>
-      </Section>
-    </Container>
+        <CountdownProvider>
+          <Section>
+            <LeftContainer>
+              <Profile />
+              <CompletedChallenges />
+              <CountDown />
+            </LeftContainer>
+
+            <RightContainer>
+              <ChallengeBox />
+            </RightContainer>
+          </Section>
+        </CountdownProvider>
+      </Container>
+    </ChallengesProvider>
   )
 }
 
 export default Home;
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  const { level, currentExperience, challengesCompleted } = ctx.req.cookies;
+
+  return {
+    props: {
+      level: Number(level),
+      currentExperience: Number(currentExperience),
+      challengesCompleted: Number(challengesCompleted)
+    }
+  }
+}
