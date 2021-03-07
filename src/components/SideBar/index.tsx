3394032fdd29d11504c9c70e09e66b4dc79ee9f0
Wilson Franca <wilson.franca.92@gmail.com>
2021-03-06 23:42:49 -0800

    feat(sidebar): got theme information and toggle function from context to add to the theme button.

--- src/components/SideBar/index.tsx
+++ src/components/SideBar/index.tsx
@@ -1,35 +1,37 @@
 import React from 'react';
 
-import Link from 'next/link';
 import { useRouter } from 'next/router';
 
 import LogoIcon from '../../assets/focus-logo.svg';
 import { FiHome, FiAward, FiSun, FiMoon, FiLogOut } from 'react-icons/fi';
-import SideBarButton from '../SideBarButton';
+import ThemeButton from '../ThemeButton';
+import SideBarLink from '../SideBarLink';
 
 import { Container, TabSection, LogoutSection } from '../../styles/components/SideBar';
 import { useAuth } from '../../contexts/auth';
+import { useTheme } from '../../contexts/theme';
 
 const SideBar: React.FunctionComponent = () => {
   const { signOut } = useAuth();
   const { asPath } = useRouter();
+  const { theme, themeName, toggleTheme } = useTheme();
 
   return (
     <Container>
       <LogoIcon />
 
       <TabSection>
-        <Link href="/" passHref>
-          <SideBarButton icon={FiHome} isActivable isActive={asPath === '/' ? true : false} />
-        </Link>
-        <SideBarButton icon={FiAward} isActivable isActive={false} />
-        <SideBarButton icon={FiSun} isActivable={false} />
+        <SideBarLink href="/" icon={FiHome} isActivable isActive={asPath === '/' ? true : false} />
+        <SideBarLink style={{ cursor: 'not-allowed' }} icon={FiAward} isActivable isActive={false} />
+        {themeName === 'light' ? (
+          <ThemeButton icon={FiSun} onClick={toggleTheme} />
+        ) : (
+            <ThemeButton icon={FiMoon} onClick={toggleTheme} />
+          )}
       </TabSection>
 
       <LogoutSection>
-        <Link href="/login">
-          <SideBarButton icon={FiLogOut} onClick={signOut} isActivable={false} />
-        </Link>
+        <SideBarLink href="/login" icon={FiLogOut} onClick={signOut} isActivable={false} />
       </LogoutSection>
     </Container>
   );
