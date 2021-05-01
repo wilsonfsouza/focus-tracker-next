4e1207b8ac04dfb30d701ee61efaf7b641e86f0d
Wilson Franca <wilson.franca.92@gmail.com>
2021-05-01 15:03:10 -0700

    feat: added scope to provider and jwt.

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -7,21 +7,29 @@ const options = {
   providers: [
     Providers.GitHub({
       clientId: process.env.GITHUB_ID,
-      clientSecret: process.env.GITHUB_SECRET
+      clientSecret: process.env.GITHUB_SECRET,
+      scope: 'read:user'
     }),
   ],
-  callbacks: {
-    signIn: (user: any, session: any) => {
-      session.id = user.id;
-      return Promise.resolve(session);
-    },
-    session: (session: any, user: any) => {
-      session.id = user.id
-      return Promise.resolve(session);
-    },
-    redirect: (_: string, _2: string) => {
-      return Promise.resolve(process.env.NEXTAUTH_URL as string)
+  jwt: {
+    signingKey: process.env.JWT_SIGNING_PRIVATE_KEY,
+    verificationOptions: {
+      maxTokenAge: `${2 * 60 * 60}s`, // 2 hours
+      algorithms: ['HS512']
     }
+  },
+  callbacks: {
+    // signIn: (user: any, session: any) => {
+    //   session.id = user.id;
+    //   return Promise.resolve(session);
+    // },
+    // session: (session: any, user: any) => {
+    //   session.id = user.id
+    //   return Promise.resolve(session);
+    // },
+    // redirect: (_: string, _2: string) => {
+    //   return Promise.resolve(process.env.NEXTAUTH_URL as string)
+    // }
   }
 }
 
