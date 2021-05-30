932d1e39d0d7a6d47643216b8cdf550255fd384c
Wilson Franca <wilson.franca.92@gmail.com>
2021-05-30 13:29:24 -0700

    feat: added config to enable coverage report.

--- jest.config.js
+++ jest.config.js
@@ -6,5 +6,13 @@ module.exports = {
   transform: {
     "^.+\\.(js|jsx|ts|tsx)$": "<rootDir>/node_modules/babel-jest"
   },
-  testEnvironment: 'jsdom'
+  testEnvironment: 'jsdom',
+  collectCoverage: true,
+  collectCoverageFrom: [
+    "src/**/*.tsx",
+    "!src/**/*.spec.tsx",
+    "!src/**/_app.tsx",
+    "!src/**/_document.tsx",
+  ],
+  coverageReporters: ['lcov', 'json'],
 };
