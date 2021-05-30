25a7b2e727cd1daf2b409a5dee06164ac4135478
Wilson Franca <wilson.franca.92@gmail.com>
2021-05-30 12:16:19 -0700

    tests: tested if button loads correctly with the right theme and if it fires the toggleTheme function when pressed.

---
+++ src/components/ThemeButton/ThemeButton.spec.tsx
@@ -0,0 +1,44 @@
+import { fireEvent, screen, render } from '@testing-library/react';
+import { renderWithTheme } from '../../tests/helpers/renderWithTheme';
+import { ThemeProvider as StyledThemeProvider } from 'styled-components';
+import { ThemeContext } from '../../hooks/theme';
+import Themes from '../../styles/theme';
+import { ThemeButton } from '.';
+
+describe('ThemeButton component', () => {
+  it('should render correctly when the light theme is selected', async () => {
+    renderWithTheme(<ThemeButton />, { themeName: 'light' });
+
+    expect(screen.getByText('Light')).toBeInTheDocument();
+  });
+
+  it('should render correctly when the dark theme is selected', async () => {
+    renderWithTheme(<ThemeButton />, { themeName: 'dark' });
+
+    expect(screen.getByText('Dark')).toBeInTheDocument();
+  });
+
+  it('should fire toggleTheme function when button is clicked', async () => {
+    const themeName = 'light';
+    const theme = Themes.light;
+    const toggleTheme = jest.fn();
+
+    render(
+      <ThemeContext.Provider value={{
+        theme,
+        themeName,
+        toggleTheme
+      }}>
+        <StyledThemeProvider theme={theme}>
+          <ThemeButton />
+        </StyledThemeProvider>
+      </ThemeContext.Provider>
+    )
+
+    const themeButton = screen.getByText('Light');
+
+    fireEvent.click(themeButton);
+
+    expect(toggleTheme).toHaveBeenCalledTimes(1);
+  });
+})
