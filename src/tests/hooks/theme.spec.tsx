dc9eafb5b38ea01c8a0b4be3303ce0f47261f674
Wilson Franca <wilson.franca.92@gmail.com>
2021-05-30 15:41:53 -0700

    tests: added tests for theme hook.

---
+++ src/tests/hooks/theme.spec.tsx
@@ -0,0 +1,45 @@
+import { renderHook, act } from '@testing-library/react-hooks';
+import Cookies from 'js-cookie';
+import { ThemeProvider, useTheme } from '../../hooks/theme';
+
+
+describe('Theme hook', () => {
+  it('should store themeName in Cookies', async () => {
+    const spyOnCookies = jest.spyOn(Cookies, 'set');
+
+    const wrapper = ({ children }) => <ThemeProvider themeName="dark">{children}</ThemeProvider>
+
+    renderHook(() => useTheme(), {
+      wrapper
+    });
+
+    expect(spyOnCookies).toHaveBeenCalledTimes(1);
+    expect(spyOnCookies).toHaveBeenCalledWith('theme', 'dark');
+  });
+
+  it('should be able to toggle from light to dark theme', async () => {
+    const { result } = renderHook(() => useTheme(), {
+      wrapper: ThemeProvider
+    });
+
+    act(() => {
+      result.current.toggleTheme()
+    });
+
+    expect(result.current.themeName).toBe('dark');
+  });
+
+  it('should be able to toggle from dark to light theme', async () => {
+    const wrapper = ({ children }) => <ThemeProvider themeName="dark">{children}</ThemeProvider>
+
+    const { result } = renderHook(() => useTheme(), {
+      wrapper
+    });
+
+    act(() => {
+      result.current.toggleTheme()
+    });
+
+    expect(result.current.themeName).toBe('light');
+  });
+})
