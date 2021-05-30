4db9cd51ee07d53c98b5e90208f9b1ceebe39ad5
Wilson Franca <wilson.franca.92@gmail.com>
2021-05-30 11:40:29 -0700

    refactor: moved the logic to switch icons depending on the active theme to the ThemeButton component instead of keeping it in the SideBar component.

--- src/components/SideBar/index.tsx
+++ src/components/SideBar/index.tsx
@@ -3,18 +3,16 @@ import React from 'react';
 import { useRouter } from 'next/router';
 
 import LogoIcon from '../../assets/focus-logo.svg';
-import { FiHome, FiAward, FiSun, FiMoon, FiLogOut } from 'react-icons/fi';
-import ThemeButton from '../ThemeButton';
+import { FiHome, FiAward, FiLogOut } from 'react-icons/fi';
+import { ThemeButton } from '../ThemeButton';
 import SideBarLink from '../SideBarLink';
 
 import { Container, TabSection, LogoutSection } from '../../styles/components/SideBar';
 import { useAuth } from '../../hooks/auth';
-import { useTheme } from '../../hooks/theme';
 
 const SideBar: React.FunctionComponent = () => {
   const { signOut } = useAuth();
   const { asPath } = useRouter();
-  const { themeName, toggleTheme } = useTheme();
 
   return (
     <Container>
@@ -23,11 +21,7 @@ const SideBar: React.FunctionComponent = () => {
       <TabSection>
         <SideBarLink href="/" icon={FiHome} isActivable isActive={asPath === '/' ? true : false} />
         <SideBarLink style={{ cursor: 'not-allowed' }} icon={FiAward} isActivable isActive={false} />
-        {themeName === 'light' ? (
-          <ThemeButton icon={FiSun} onClick={toggleTheme} />
-        ) : (
-          <ThemeButton icon={FiMoon} onClick={toggleTheme} />
-        )}
+        <ThemeButton />
       </TabSection>
 
       <LogoutSection>
