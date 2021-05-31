0bc3862f97b40e6307841b10fc4f70d9ad30df71
Wilson Franca <wilson.franca.92@gmail.com>
2021-05-31 16:39:16 -0700

    tests: checked if function from challenges context is fired when pressing the close button.

--- src/components/LevelUpModal/LevelUpModal.spec.tsx
+++ src/components/LevelUpModal/LevelUpModal.spec.tsx
@@ -1,4 +1,4 @@
-import { screen } from '@testing-library/react';
+import { fireEvent, screen } from '@testing-library/react';
 import { LevelUpModal } from '.';
 import { ThemeProvider } from '../../hooks/theme';
 import { renderWithChallengesContext } from '../../tests/helpers/renderWithChallengesContext';
@@ -14,10 +14,29 @@ describe('LevelUpModal component', () => {
       }
     }
 
-    renderWithChallengesContext(<LevelUpModal />, { wrapper: themeWrapper, providerProps })
+    renderWithChallengesContext(<LevelUpModal />, { wrapper: themeWrapper, providerProps });
 
     expect(screen.getByText('1')).toBeInTheDocument();
     expect(screen.getByText('Congratulations')).toBeInTheDocument();
     expect(screen.getByText('You reached a new level')).toBeInTheDocument();
-  })
+  });
+
+  it('should fire a function to close modal when pressing the x button', async () => {
+    const themeWrapper = ({ children }) => <ThemeProvider themeName="light">{children}</ThemeProvider>
+
+    const providerProps = {
+      value: {
+        level: 1,
+        handleCloseLevelUpModal: jest.fn(),
+      }
+    }
+
+    renderWithChallengesContext(<LevelUpModal />, { wrapper: themeWrapper, providerProps });
+
+    const closeModalButton = screen.getByTestId('close-button-modal');
+
+    fireEvent.click(closeModalButton);
+
+    expect(providerProps.value.handleCloseLevelUpModal).toHaveBeenCalledTimes(1);
+  });
 })
