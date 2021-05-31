0cb14dc2188abe5b52ccbb4db66ae361afa93351
Wilson Franca <wilson.franca.92@gmail.com>
2021-05-31 16:57:24 -0700

    tests: tested if component renders correctly when user information is available.

---
+++ src/components/Profile/Profile.spec.tsx
@@ -0,0 +1,32 @@
+import { screen } from '@testing-library/react';
+import Profile from '.';
+import { ThemeProvider } from '../../hooks/theme';
+import { renderWithChallengesContext } from '../../tests/helpers/renderWithChallengesContext';
+
+describe('Profile component', () => {
+  it('should render correctly when user has logged in', async () => {
+    const themeWrapper = ({ children }) => <ThemeProvider themeName="light">{children}</ThemeProvider>
+
+    const providerProps = {
+      value: {
+        level: 1
+      }
+    }
+
+    const user = {
+      name: 'Jane Doe',
+      imageUrl: 'fake-imageUrl'
+    }
+
+    renderWithChallengesContext(<Profile user={user} />, { wrapper: themeWrapper, providerProps });
+
+    const userImage = screen.getByAltText(user.name);
+
+    expect(screen.getByText(user.name)).toBeInTheDocument();
+
+    expect(userImage).toBeInTheDocument();
+    expect(userImage).toHaveAttribute('src', user.imageUrl);
+
+    expect(screen.getByText(/1/)).toBeInTheDocument();
+  });
+})
