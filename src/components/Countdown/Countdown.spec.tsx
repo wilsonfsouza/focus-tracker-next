665a5f9280c0a266765a58acf034a79cd8f293d6
Wilson Franca <wilson.franca.92@gmail.com>
2021-05-31 23:16:04 -0700

    tests: tested initial render of countdown.

---
+++ src/components/Countdown/Countdown.spec.tsx
@@ -0,0 +1,35 @@
+import { screen } from '@testing-library/react';
+import { Countdown } from '.';
+import { renderWithTheme } from '../../tests/helpers/renderWithTheme';
+import { wrapWithCountdownContext } from '../../tests/helpers/wrapWithCountdownContext';
+
+describe('Countdown component', () => {
+  it('should render the countdown with initial time when it is not active', async () => {
+    const themeProviderProps = {
+      themeName: 'light'
+    }
+
+    const countdownProviderProps = {
+      value: {
+        minutes: 25,
+        seconds: 0,
+        hasFinished: false,
+        isActive: false,
+        resetCountDown: jest.fn(),
+        startCountDown: jest.fn()
+      }
+    }
+
+    renderWithTheme(
+      wrapWithCountdownContext(<Countdown />, { providerProps: countdownProviderProps }),
+      { providerProps: themeProviderProps }
+    );
+
+    expect(screen.getByTestId('minuteLeft').textContent).toBe('2');
+    expect(screen.getByTestId('minuteRight').textContent).toBe('5');
+    expect(screen.getByText(/:/)).toBeInTheDocument();
+    expect(screen.getByTestId('secondLeft').textContent).toBe('0');
+    expect(screen.getByTestId('secondRight').textContent).toBe('0');
+    expect(screen.getByText('Start')).toBeInTheDocument();
+  });
+})
