c22adb7e1d52b5c9a03e5bb9a017d9282b955507
Wilson Franca <wilson.franca.92@gmail.com>
2021-06-01 20:16:08 -0700

    fix: removed React.FC and changed thumbnail format to png.

--- src/components/SEO/index.tsx
+++ src/components/SEO/index.tsx
@@ -1,35 +1,37 @@
-import React from 'react';
 import Head from 'next/head';
 
 interface SEOProps {
   title: string;
 }
 
-const SEO: React.FunctionComponent<SEOProps> = ({ title }) => {
+export default function SEO({ title }: SEOProps) {
   return (
     <Head>
       <title>Smart.it | {title}</title>
 
       <link rel="shortcut icon" href="/favicon.png" />
-      <meta name="keywords" content="wilsonfsouza, focus, move" />
+      <meta name="title" content={`Smart.it | ${title}`} />
+      <meta name="keywords" content="Wilson Franca de Souza, focus, move" />
       <meta name="description" content="An app to help you balance your focus sessions with ergonomic movements." />
 
+      {/* <!-- Open Graph / Facebook --> */}
+      <meta property="og:type" content="website" />
       <meta property="og:site_name" content="Smart.it" />
-      <meta property="og:title" content="Smart.it" />
+      <meta property="og:title" content={`Smart.it | ${title}`} />
       <meta property="og:description" content="An app to help you balance your focus sessions with ergonomic movements." />
-      <meta property="og:image" content="/thumb.svg" />
-      <meta property="og:image:type" content="image/svg" />
+      <meta property="og:image" content={`${process.env.NEXTAUTH_URL}/thumb.png`} />
+      <meta property="og:image:type" content="image/png" />
       <meta property="og:image:width" content="1200" />
       <meta property="og:image:height" content="630" />
 
-      <meta name="twitter:image" content="/thumb.svg" />
-      <meta name="twiiter:image:alt" content="Thumbnail" />
+      {/* <!-- Twitter --> */}
       <meta name="twitter:card" content="summary_large_image" />
-      <meta name="twitter:title" content="Smart.it" />
+      <meta property="twitter:url" content={process.env.NEXTAUTH_URL} />
+      <meta property="twitter:title" content={`Smart.it | ${title}`} />
+      <meta name="twitter:image" content={`${process.env.NEXTAUTH_URL}/thumb.png`} />
+      <meta name="twiiter:image:alt" content="Thumbnail" />
       <meta name="twitter:description" content="An app to help you balance your focus sessions with ergonomic movements." />
-      <meta name="twiiter:create" content="wilsonfsouza" />
+      <meta name="twiiter:create" content="Wilson Franca de Souza" />
     </Head>
   );
 }
-
-export default SEO;
