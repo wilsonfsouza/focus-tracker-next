77b528ba60f30ccf7243c6d766bf1ef841b3ca94
Wilson Franca <wilson.franca.92@gmail.com>
2021-06-01 20:18:19 -0700

    chore: removed unnecessary imports.

--- src/contexts/index.tsx
+++ src/contexts/index.tsx
@@ -1,4 +1,4 @@
-import React, { ReactNode } from 'react';
+import { ReactNode } from 'react';
 import { ThemeProvider, ThemeOptions } from '../hooks/theme';
 import { Session } from 'next-auth/client';
 
