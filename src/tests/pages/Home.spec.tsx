a20283e6b9e3ecf5ada6f0edad4e6444edc12d31
Wilson Franca <wilson.franca.92@gmail.com>
2021-06-02 20:02:14 -0700

    tests: return initial data with light theme when theme is not defined in the cookies yet.

--- src/tests/pages/Home.spec.tsx
+++ src/tests/pages/Home.spec.tsx
@@ -33,7 +33,39 @@ describe('Home page', () => {
       currentExperience: "0"
     });
 
-    const req = { cookies: { theme: 'light' } }
+    const req = { cookies: { theme: 'dark' } }
+
+    const response = await getServerSideProps({ req } as any);
+
+    const expectedResponse = {
+      props: {
+        level: 1,
+        currentExperience: 0,
+        challengesCompleted: 0,
+        theme: 'dark'
+      }
+    }
+
+    expect(response).toEqual(
+      expect.objectContaining(expectedResponse)
+    );
+  });
+
+  it('should load initial data with light theme when the theme is not stored in the cookies yet', async () => {
+    const mockedSession = mocked(getSession);
+    mockedSession.mockResolvedValueOnce({
+      user: { name: 'Jane Doe', email: 'janedoe@example.com' },
+      expires: 'fake-expires'
+    });
+
+    const mockedUserData = mocked(getUserData);
+    mockedUserData.mockResolvedValueOnce({
+      level: "1",
+      challengesCompleted: "0",
+      currentExperience: "0"
+    });
+
+    const req = { cookies: {} }
 
     const response = await getServerSideProps({ req } as any);
 
